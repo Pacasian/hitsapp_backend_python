5e4b4db394f21e59d303b8d664025a66993cf4ec
Srijeet Gopalan <srijeet@Srijeets-MacBook-Air.local>
2021-12-14 21:35:38 +0000

    test

--- login_signup.py
+++ login_signup.py
@@ -43,7 +43,7 @@ def login(usr, pwd):
             "name": pwd
         }       
         upd={
-            "token_val": accTo
+            "token_val": {'Value' : accTo}
         }
 
         table.update_item(Key=key, AttributeUpdates=upd)
