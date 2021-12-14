fcb1154b09963ff01833754798785fcfb391d229
Srijeet Gopalan <srijeet@Srijeets-MacBook-Air.local>
2021-12-14 17:18:10 +0000

    test

--- login_signup.py
+++ login_signup.py
@@ -7,18 +7,33 @@ from awscli.errorhandler import ClientError
 
 table= connect_table.table1
 cog = connect_table.cogclient
+COGNITO_USER_CLIENT_ID = "riiugb6k7m01m734ctvt420vv"
 
 # Login Block
 def login(usr, pwd):
     try:
-        response = table.get_item(
-            Key={
-                "userName": usr,
-                "password": pwd
+        # response = table.get_item(
+        #     Key={
+        #         "userName": usr,
+        #         "password": pwd
 
-            }
+        #     }
+        # )
+
+        response = cog.initiate_auth(
+        ClientId=COGNITO_USER_CLIENT_ID,
+        AuthFlow="USER_PASSWORD_AUTH",
+        AuthParameters={"USERNAME": usr, "PASSWORD": pwd},
         )
-        return json.loads(json.dumps(response['Item'], indent=4))
+        accTo = response["AuthenticationResult"]["AccessToken"]
+        response = cog.get_user(AccessToken=accTo)
+        res = json.loads(json.dumps(response))
+        # print(res['Username'])
+        # for i in range(0,len(res['UserAttributes'])):
+        #     if(res['UserAttributes'][i]['Name']=="phone_number" or res['UserAttributes'][i]['Name']=="name"):
+        #         print(res['UserAttributes'][i]['Value'])
+
+        return json.loads(json.dumps(response))
     except ClientError as e:
         print(e.response['Error']['Message'])
         return {"success": "false"}
@@ -34,7 +49,7 @@ def signUp(usr,pwd,name,mob):
         #     "mob_number": mob,
         # })
 
-        COGNITO_USER_CLIENT_ID = "riiugb6k7m01m734ctvt420vv"
+        
         cog.sign_up(
             ClientId=COGNITO_USER_CLIENT_ID,
             Username=usr,
